a5320bcc8363d1b469762acdacc8de206faf6abc
sameer <sameersahu200138@gmail.com>
2024-03-18 14:18:59 +0530

    switched to hash router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 import Paste from './components/Paste.tsx'
 import Code from './components/Code.tsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
@@ -26,6 +26,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
